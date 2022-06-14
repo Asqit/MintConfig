a251b04f04858205c157ef43dd68236a3644f5dd
Ondřej Tuček <70013396+Asqit@users.noreply.github.com>
2022-06-14 09:00:51 +0200

    adding base files

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This repository serves the one and only purpose and that is being setting up my new Linux Mint.
 
 It will install basic software I use everyday.
+
+![my desktop](showcase.png)
---
+++ install.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Theme --------------------------------------
+cd ~/Downloads
+
+tar -xf Mint-Darcula.tar.gz
+
+cd Mint-Darcula
+
+cd theme 
+
+mv darcula ~/.themes
+
+cd ..
+
+cd icons
+
+tar -xf Colloid-purple.tar.xz
+tar -xf WhiteSur-cursors.tar.xz
+
+mkdir ~/.icons
+
+mv Colloid-purple/* ~/.icons
+mv WhiteSur-cursors ~/.icons
+
+# Gnome terminal (dracula theme) -------------
+cd ~/
+sudo apt-get install dconf-cli
+git clone https://github.com/dracula/gnome-terminal
+cd gnome-terminal
+chmod +x install.sh
+./install.sh
+cd .. 
+rm -rf gnome-terminal
+
+# Plank theme --------------------------------
+cd ~/
+git clone https://github.com/dracula/plank.git
+cd plank
+mv Dracula ~/.local/share/plank/themes
+cd ..
+rm -rf plank
+
+# LibreOffice --------------------------------
+cd ~/
+git clone https://github.com/dracula/libreoffice.git
+cd libreoffice/
+mv dracula.soc ~/.config/libreoffice/*/user/config
+cd ..
+rm -rf libreoffice
+
+
+
+# Update & upgrade ----------------------------
+sudo apt-get update 
+sudo apt-get upgrade
+
+# Installing software -------------------------
+sudo apt install vim
+sudo apt install vlc 
+sudi apt install plank 
+sudo apt instal tlp
+sudo apt install tlpui
+sudo apt install git 
+sudo apt install g++
+sudo apt install steam 
+sudo apt install gimp 
+sudo apt install mc 
+sudo apt install nmap 
+sudo apt install geany
+sudo apt install python3-pip
+
+# Installing GitHub Desktop -------------------
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
+
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
+
+sudo apt-get update
+
+sudo apt install github-desktop
+
+
+
+# Installing visual studio code ---------------
+sudo apt-get install wget gpg
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+
+sudo apt update 
+
+sudo apt install code
+
+
+# Install node version manager --------------
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install 18.3.0
+
+echo "All done...goodbye"
