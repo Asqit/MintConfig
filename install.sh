cbdf2d05310bfa5da6fb56874c7f19d8785e3d9c
Ondřej Tuček <70013396+Asqit@users.noreply.github.com>
2022-06-16 15:35:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,7 +79,8 @@ mv init.vim ~/.config/nvim/
 sh -c 'curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-echo alias nvim=\"vim\" | cat >> sample.txt 
+
+echo alias nvim=\"vim\" | cat >> .bashrc
 
 
 # Installing GitHub Desktop -------------------
